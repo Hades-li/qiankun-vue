0a2403b5aac961a6779b27c80f76d555f60060cf
plusli <121318100@qq.com>
2020-04-19 22:28:19 +0800

    v0.2.0

---
+++ src/components/framework/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="framework">
+    <div id="subApp"></div>
+  </div>
+</template>
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+
+  @Component({
+    name: 'Framework'
+  })
+  export declare class Framework extends Vue {
+
+  }
+</script>
